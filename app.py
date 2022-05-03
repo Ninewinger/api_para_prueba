e471ea698dd9bb8b94a4e77285a07aebd4eafb00
Diego Vergara <71197696+Ninewinger@users.noreply.github.com>
2022-05-03 15:42:33 -0400

    creo que ahora si

--- app.py
+++ app.py
@@ -2,18 +2,17 @@ from operator import index
 import pandas as pd
 import json
 
-df = pd.read_excel("C:/Users/x_zer/Documents/intento 2/prueba entrevista/202112_PUB_TRINT.xlsb", engine='pyxlsb', index=False)
+""" df = pd.read_excel("C:/Users/x_zer/Documents/intento 2/prueba entrevista/202112_PUB_TRINT.xlsb", engine='pyxlsb')
 
-#remove nan and unnamed columns
-df = df.dropna(axis=1, how='all')
-df = df.dropna(axis=0, how='all')
+#remove nan
+df = df.dropna(axis=0, how='any')
 
-df.to_csv("C:/Users/x_zer/Documents/intento 2/prueba entrevista/202112_PUB_TRINT.csv", index=False, encoding='utf-8-sig')
+df.to_csv("C:/Users/x_zer/Documents/intento 2/prueba entrevista/202112_PUB_TRINT.csv", index=False, encoding='utf-8-sig', header=False, index_label=False) """
 
-on_csv = pd.read_csv("C:/Users/x_zer/Documents/intento 2/prueba entrevista/202112_PUB_TRINT.csv", encoding='utf-8-sig', index=False)
+on_csv = pd.read_csv("C:/Users/x_zer/Documents/intento 2/prueba entrevista/202112_PUB_TRINT.csv", encoding='utf-8-sig')
 
+on_csv.to_json("C:/Users/x_zer/Documents/intento 2/prueba entrevista/202112_PUB_TRINT.json", orient='records', force_ascii=False)
 
+on_json = pd.read_json("C:/Users/x_zer/Documents/intento 2/prueba entrevista/202112_PUB_TRINT.json", orient='records', encoding='utf-8-sig')
 
-
-
-print(on_csv)
+print(on_json)
